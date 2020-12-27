36a60d5e3bbb34b1a16aeb0b6fd8155bbeec92fe
Michael Doctor <michaelcdoctor@gmail.com>
2020-12-27 14:16:20 -0700

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home.page';
 import './App.css'
+import NotFound from './pages/NotFound.page';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
---
+++ src/pages/NotFound.page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const NotFound = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="utility-page-wrap">
+        <div className="utility-page-content w-form">
+          <img
+            src="https://d3e54v103j8qbb.cloudfront.net/static/page-not-found.211a85e40c.svg"
+            alt="Not Found"
+          />
+          <h2>Page Not Found</h2>
+          <div>
+            The page you are looking for doesn&#x27;t exist or has been moved.
+          </div>
+          <a href="/" className="profile-side-btn w-button">
+            Return Home
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default NotFound;
