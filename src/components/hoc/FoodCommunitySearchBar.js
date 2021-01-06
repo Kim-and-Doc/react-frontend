00ef906de1cc6703b048c06306f671a1355f05e2
SooeunKim <sooeuk08@gmail.com>
2021-01-06 08:38:06 -0700

    Edited FoodCom side bar component

--- src/components/hoc/FoodCommunitySearchBar.js
+++ src/components/hoc/FoodCommunitySearchBar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const FoodCommunitySearchBar = () => {
   return (
-    <div class="main-content-search-bar-area">
-        <div class="main-content-search-bar">
-          <form action="/search" class="main-content-search-bar w-form"><input type="search" class="main-content-search-bar-text-field w-input" maxlength="256" name="query" placeholder="Search for any post" id="search" required="" /><input type="submit" value="Search" class="main-content-search-bar-search-button w-button" /></form>
+    <div className="main-content-search-bar-area">
+        <div className="main-content-search-bar">
+          <form action="/search" className="main-content-search-bar w-form"><input type="search" className="main-content-search-bar-text-field w-input" maxlength="256" name="query" placeholder="Search for any post" id="search" required="" /><input type="submit" value="Search" className="main-content-search-bar-search-button w-button" /></form>
         </div>
       </div>
   );
--- src/components/sidebar/FoodCommunitySideBar.js
+++ src/components/sidebar/FoodCommunitySideBar.js
@@ -2,26 +2,17 @@ import React from 'react';
 
 const FoodCommunitySideBar = () => {
   return (
-    <div className="main-content-right-sidebar">
-      <ul className="right-sidebar-list w-list-unstyled">
-        <li className="list-item-upload">
-          <a href="/404" className="right-sidebar-upload-button w-button">
-            Upload Posts
-          </a>
-        </li>
-        <li className="list-item-snf-button">
-          <a
-            href="/404"
-            className="right-sidebar-mycontribution-button w-button"
-          >
-            My SNF
-            <br />
-            Contribution Button
-            <br />
-          </a>
-        </li>
-      </ul>
-    </div>
+    <div className="main-content-right-sidebar-area">
+        <div className="main-content-right-sidebar-upload-button-area">
+          <a href="/404" className="right-sidebar-upload-button w-button">Upload Post</a>
+        </div>
+        <div className="main-content-right-sidebar-my-contribution-button-area">
+          <a href="/404" className="right-sidebar-my-contribution-button w-button">View My Post</a>
+        </div>
+        <div className="main-content-right-sidebar-filter-area">
+          <a href="/404" className="right-sidebar-filter-box w-button">Filter??</a>
+        </div>
+      </div>
   );
 };
 
