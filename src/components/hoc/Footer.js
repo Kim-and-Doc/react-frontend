90e3f3c3d1fae9ef1dd408874d2028ccaff341bc
SooeunKim <sooeuk08@gmail.com>
2021-01-06 08:41:28 -0700

    Edited footer component

--- src/components/hoc/Footer.js
+++ src/components/hoc/Footer.js
@@ -2,66 +2,25 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="bottom-company-info-area">
-        <div className="company-logo-text">SNF</div>
-        <div className="bottom-org-info-text-area">
-          <p className="bottom-org-info-text">
-            Kim-and-Doc is blah blah blah..........
-            <br />
-            Brief company info (such as location is Calgary ....)
-          </p>
-        </div>
-        <div className="bottom-org-linkedin-logo-area">
-          <img
-            src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
-            loading="lazy"
-            alt=""
-            className="bottom-icon"
-          />
-        </div>
-      </div>
-      <div className="bottom-indiv-info-area">
-        <div className="bottom-indiv-info-text-area">
-          <p className="borrom-indiv-info-text">
-            Brief personal info (such as location is Calgary ....)
-          </p>
-          <div className="bottom-individual-logo-area">
-            <img
-              src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
-              loading="lazy"
-              alt=""
-              className="bottom-icon"
-            />
-            <img
-              src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
-              loading="lazy"
-              alt=""
-              className="bottom-icon"
-            />
+    <div class="footer">
+        <div class="bottom-company-info-area">
+          <div class="company-logo-text">SNF</div>
+          <div class="bottom-org-info-text-area">
+            <p class="bottom-org-info-text">Kim-and-Doc is blah blah blah..........<br/>Brief company info (such as location is Calgary ....)</p>
           </div>
+          <div class="bottom-org-linkedin-logo-area"><img src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg" loading="lazy" alt="" class="bottom-icon" /></div>
         </div>
-        <div className="bottom-indiv-info-text-area">
-          <p className="borrom-indiv-info-text">
-            Brief personal info (such as location is Calgary ....)
-          </p>
-          <div className="bottom-individual-logo-area">
-            <img
-              src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
-              loading="lazy"
-              alt=""
-              className="bottom-icon"
-            />
-            <img
-              src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
-              loading="lazy"
-              alt=""
-              className="bottom-icon"
-            />
+        <div class="bottom-indiv-info-area">
+          <div class="bottom-indiv-info-text-area">
+            <p class="borrom-indiv-info-text">Brief personal info (such as location is Calgary ....)</p>
+            <div class="bottom-individual-logo-area"><img src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg" loading="lazy" alt="" class="bottom-icon" /><img src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg" loading="lazy" alt="" class="bottom-icon" /></div>
+          </div>
+          <div class="bottom-indiv-info-text-area">
+            <p class="borrom-indiv-info-text">Brief personal info (such as location is Calgary ....)</p>
+            <div class="bottom-individual-logo-area"><img src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg" loading="lazy" alt="" class="bottom-icon" /><img src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg" loading="lazy" alt="" class="bottom-icon" /></div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
